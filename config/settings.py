a2e89ee5a6c8794444045d1cb71e796fa75c8d26
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2021-10-05 23:06:23 -0500

    Add default key for deployment

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+
+from django.core.management.utils import get_random_secret_key
 from environs import Env
 
 env = Env()
@@ -12,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env("DJANGO_SECRET_KEY", default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
